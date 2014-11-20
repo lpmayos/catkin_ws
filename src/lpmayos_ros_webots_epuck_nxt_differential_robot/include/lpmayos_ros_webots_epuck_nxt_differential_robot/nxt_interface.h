fcc09bdc0a67378c45c5cac19879251144735618
lpmayos <lpmayo@gmail.com>
2014-11-20 23:00:45 +0100

    indentatio, pep8...

--- src/lpmayos_ros_webots_epuck_nxt_differential_robot/include/lpmayos_ros_webots_epuck_nxt_differential_robot/nxt_interface.h
+++ src/lpmayos_ros_webots_epuck_nxt_differential_robot/include/lpmayos_ros_webots_epuck_nxt_differential_robot/nxt_interface.h
@@ -1,70 +1,3 @@
-/**
- * \file  nxt_interface.h
- *  nxt_interface.h
- *  Created by
- *  \author Vlad Estivill-Castro
- *  \date 14/10/2014.
- */
-
-/*
- *
- * This is module acts as a bridge between the ROS and a LEGO Mindstorm.
- * Sensors can be commanded to post updates and actuators can be
- * controlled. 
- *
- *  Copyright 2014 Vlad Estivill-Castro. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials
- *    provided with the distribution.
- *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgement:
- *
- *        This product includes software developed by Rene Hexel.
- *
- * 4. Neither the name of the author nor the names of contributors
- *    may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * -----------------------------------------------------------------------
- * This program is free software; you can redistribute it and/or
- * modify it under the above terms or under the terms of the GNU
- * General Public License as published by the Free Software Foundation;
- * either version 2 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see http://www.gnu.org/licenses/
- * or write to the Free Software Foundation, Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA  02110-1301, USA.
- *
- */
-
 #include "r2d2_base.h"
 #pragma clang diagnostic ignored "-Wold-style-cast"
 #include "usb.h"
@@ -76,29 +9,28 @@
 #include "lpmayos_ros_webots_epuck_nxt_differential_robot/Rrobot.h"
 #include "lpmayos_ros_webots_epuck_nxt_differential_robot/RbuttonStatus.h"
 
-class NXT_interface
-{
-         public:
-           ///< constructor
-        NXT_interface();
+class NXT_interface {
+public:
+    ///< constructor
+    NXT_interface();
 
-        std::string banner() { return std::string("(c) Vlad Estivill_Castro, demo subscriber R@D@-NXT ROS driver");}
+    void run(int argc, char **argv);
 
-	void run(int argc, char **argv);
+    /// call-back method robot
+    void robotCallback(const lpmayos_ros_webots_epuck_nxt_differential_robot::Rrobot::ConstPtr& msg);
 
-        /// call-back method robot
-void   robotCallback(const lpmayos_ros_webots_epuck_nxt_differential_robot::Rrobot::ConstPtr& msg);
+    /// call-back method button status/value
+    bool value_buttonCallback(
+        lpmayos_ros_webots_epuck_nxt_differential_robot::RbuttonStatus::Request & req,
+        lpmayos_ros_webots_epuck_nxt_differential_robot::RbuttonStatus::Response& res);
 
-        /// call-back method button status/value
-	bool value_buttonCallback(
-             lpmayos_ros_webots_epuck_nxt_differential_robot::RbuttonStatus::Request & req,
-             lpmayos_ros_webots_epuck_nxt_differential_robot::RbuttonStatus::Response& res);
-
-	private:
-	        r2d2::Brick* brick;
-		r2d2::NXT* nxt;
-		r2d2::Sensor* sensor_left; r2d2::Sensor* sensor_right;
-		bool status_sensor_left; bool status_sensor_right;
-
-		r2d2::Motor* motor_right; r2d2::Motor* motor_left;
+private:
+    r2d2::Brick* brick;
+    r2d2::NXT* nxt;
+    r2d2::Sensor* sensor_left;
+    r2d2::Sensor* sensor_right;
+    bool status_sensor_left;
+    bool status_sensor_right;
+    r2d2::Motor* motor_right;
+    r2d2::Motor* motor_left;
 };
